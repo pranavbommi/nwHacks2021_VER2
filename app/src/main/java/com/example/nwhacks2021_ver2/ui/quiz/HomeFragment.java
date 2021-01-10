0c75ffa6a32ae0b0d335b3d5963a78f1475f0a46
ishaan2020 <53574413+ishaan2020@users.noreply.github.com>
2021-01-10 01:17:29 -0500

    made questions work

--- app/src/main/java/com/example/nwhacks2021_ver2/ui/quiz/HomeFragment.java
+++ app/src/main/java/com/example/nwhacks2021_ver2/ui/quiz/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.nwhacks2021_ver2.ui.quiz;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,8 +19,22 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.nwhacks2021_ver2.R;
+import com.example.nwhacks2021_ver2.userDataObject;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    ArrayList<userDataObject> ALL_USER_DATA = new ArrayList<>();
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String USER_DATA_KEY = "USER_DATA";
+
+    String userFirstQuestionAnswer;
+    int userPointSum = 0;
 
     private HomeViewModel homeViewModel;
 
@@ -30,6 +47,8 @@ public class HomeFragment extends Fragment {
 
     private TextView finishedIndicator;
 
+    private Button okButton;
+
     private Button option1;
     private Button option2;
     private Button option3;
@@ -39,6 +58,7 @@ public class HomeFragment extends Fragment {
     private int qCount = 1;
 
     private String[][] buttonOptions = {{""}, {"Horrible","Not the best","Not good, not bad","Amazing"}, {"Nothing in the tank","Comes in bursts","Haven’t really noticed","Had quite a bit"}, {"I was zoned out the whole day", "My focus was going in and out", "My focus was alright", "I was attentive the whole day"}, {"None", "Not many", "I have had some", "Way too many"}};
+    private int[][] points = {{0},{1,2,3,4},{1,2,3,4}, {1,2,3,4}, {4,3,2,1}};
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -57,6 +77,7 @@ public class HomeFragment extends Fragment {
         option2 = root.findViewById(R.id.option2);
         option3 = root.findViewById(R.id.option3);
         option4 = root.findViewById(R.id.option4);
+        okButton = root.findViewById(R.id.okButton);
         finishedIndicator = root.findViewById(R.id.finishedIndicator);
 
         progressBar.setMax(questions.length);
@@ -75,6 +96,7 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 qCount++;
+                userFirstQuestionAnswer = inputField.getText().toString();
                 displayUI();
             }
         });
@@ -82,6 +104,7 @@ public class HomeFragment extends Fragment {
         option1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userPointSum += points[qCount-1][0];
                 qCount++;
                 displayUI();
             }
@@ -89,6 +112,7 @@ public class HomeFragment extends Fragment {
         option2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userPointSum += points[qCount-1][1];
                 qCount++;
                 displayUI();
             }
@@ -96,6 +120,7 @@ public class HomeFragment extends Fragment {
         option3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userPointSum += points[qCount-1][2];
                 qCount++;
                 displayUI();
             }
@@ -103,8 +128,18 @@ public class HomeFragment extends Fragment {
         option4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userPointSum += points[qCount-1][3];
                 qCount++;
                 displayUI();
+            }
+        });
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userDataObject uDO = new userDataObject(userFirstQuestionAnswer, userPointSum);
+                ALL_USER_DATA.add(uDO);
+                saveData();
             }
         });
 
@@ -148,6 +183,7 @@ public class HomeFragment extends Fragment {
         }
         else{
             finishedIndicator.setVisibility(View.VISIBLE);
+            okButton.setVisibility(View.VISIBLE);
 
             questionNumber.setVisibility(View.GONE);
             maxQuestionNumberText.setVisibility(View.GONE);
@@ -158,9 +194,33 @@ public class HomeFragment extends Fragment {
             option2.setVisibility(View.GONE);
             option3.setVisibility(View.GONE);
             option4.setVisibility(View.GONE);
+
+
         }
     }
 
+    private void saveData(){
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Gson gson = new Gson();
+        String json = gson.toJson(ALL_USER_DATA);
+        editor.putString(USER_DATA_KEY, json);
+        editor.apply();
+
+    }
+    private void loadData(){
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(USER_DATA_KEY, null);
+        Type type = new TypeToken<ArrayList<userDataObject>>() {}.getType();
+
+        ALL_USER_DATA = gson.fromJson(json, type);
+
+        if(ALL_USER_DATA == null){
+            ALL_USER_DATA = new ArrayList<userDataObject>();
+        }
+    }
 
 
 
---
+++ app/src/main/java/com/example/nwhacks2021_ver2/userDataObject.java
@@ -0,0 +1,11 @@
+package com.example.nwhacks2021_ver2;
+
+public class userDataObject {
+    private String userInputFirstQuestion;
+    private int userScore;
+
+    public userDataObject(String userInputFirstQuestion, int userScore){
+        this.userInputFirstQuestion = userInputFirstQuestion;
+        this.userScore = userScore;
+    }
+}
